3abcd392edc5ab657cab6a277cbd18347a50f09a
Johann <johanson88@gmail.com>
2020-03-08 22:22:45 +0100

    add style footer

---
+++ index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', _ => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', _ => {
+        document.body.classList.remove('nav-open');
+    })
+})
